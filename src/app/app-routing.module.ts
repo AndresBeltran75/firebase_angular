4b26ff6e3b2d2660ed60588dfb69f470fb3a3491
Andres Beltran <stiedan@gmail.com>
2022-06-29 23:05:44 -0500

    Se incluyen rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CargaArchivosComponent } from './carga-archivos/carga-archivos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: CargaArchivosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
