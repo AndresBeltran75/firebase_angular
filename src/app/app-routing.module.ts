256336e96bf45081348c4c9124ce78af2c465117
Andres Beltran <stiedan@gmail.com>
2022-06-30 21:33:06 -0500

    Se eliminan import sin utilizar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CargaArchivosComponent } from './carga-archivos/carga-archivos.component';
 import { HomeComponent } from './home/home.component';
-import { TabsComponent } from './tabs/tabs.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
